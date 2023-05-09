3536bd9dcfd8dd2c4716811ad3039423d84e1fc0
Andrii Honcharenko <taburyak@gmail.com>
2023-05-09 10:29:05 +0300

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # IoT_Smart_Watch
+
+# Project Description
+
+This project is based on the ESP8266 or ESP32 WiFi module, TFT display, temperature and humidity sensor, such as DHT11(22), BUZZER (optional), RTC module (optional).
+
+Functionality:
+- Display current time, calendar (synchronized with a time server), alarm (optional)
+- Display temperature and humidity from the DHT sensor, local weather forecast
+- Display the number of subscribers, views, and videos from your YouTube channel (optional)
+- Custom web server on the microcontroller for configuration settings
+- Over-the-air firmware update (optional)
+
+Mandatory features:
+- Device can be configured via WiFi by connecting to its access point
+- Device can connect to any WiFi network for internet access
+- TFT display shows:
+    - Time, calendar (date, month, year, day of the week) taken from a time service on the internet
+    - Temperature and humidity from the DHT sensor, weather forecast from a weather service
+    - The number of subscribers, views, and videos on your YouTube channel (enabled in settings)
+
+Optional features:
+- RTC module
+- Alarm clock
+- Over-the-air firmware update
+- Temperature and humidity readings are sent to a server such as https://thingspeak.com/
--- include/TFTDisplay.h
+++ include/TFTDisplay.h
@@ -5,12 +5,12 @@
 #include <SPI.h>
 #include "SettingsIoT.h"
 
-#define TFT_CS                  5U   // PyBadge/PyGamer display control pins: chip select
-#define TFT_RST                 13U  // Display reset
-#define TFT_DC                  12U  // Display data/command select
-#define TFT_BACKLIGHT           14U  // Display backlight pin
-#define TFT_MOSI                23U  // Data out
-#define TFT_SCLK                18U  // Clock out
+#define TFT_CS                  GPIO_NUM_5   // PyBadge/PyGamer display control pins: chip select
+#define TFT_RST                 GPIO_NUM_13  // Display reset
+#define TFT_DC                  GPIO_NUM_12  // Display data/command select
+#define TFT_BACKLIGHT           GPIO_NUM_14  // Display backlight pin
+#define TFT_MOSI                GPIO_NUM_23  // Data out
+#define TFT_SCLK                GPIO_NUM_18  // Clock out
 
 class TFTDisplay
 {
@@ -30,8 +30,11 @@ TFTDisplay::TFTDisplay()
 
 inline void TFTDisplay::Init()
 {
-    pinMode(TFT_BACKLIGHT, OUTPUT);
-    digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
+    gpio_config_t blPin;
+    blPin.mode = GPIO_MODE_OUTPUT;
+    blPin.pin_bit_mask = (1ULL << TFT_BACKLIGHT);
+    gpio_config(&blPin);
+    gpio_set_level(TFT_BACKLIGHT, HIGH);
 
     tft.initR(INITR_GREENTAB);
     tft.setRotation(0);
@@ -40,10 +43,18 @@ inline void TFTDisplay::Init()
 
 inline void TFTDisplay::Welcome()
 {
-    tft.setCursor(0, 0);
-    tft.setTextColor(ST77XX_YELLOW);
-    tft.setTextSize(1);
-    tft.println("Welcome!");
+    int16_t x, y;
+    uint16_t w, h;
+
+    String bufStr;
+
+    bufStr = String("IoT Smart Clock " + String(BOARD_FIRMWARE_VERSION));
+
+    tft.setTextSize(1U);
+    tft.getTextBounds(bufStr, 0, 0, &x, &y, &w, &h);
+    tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
+    tft.setCursor((ST7735_TFTWIDTH_128 / 2) - (w / 2), (ST7735_TFTHEIGHT_160 / 2) - (h / 2));
+    tft.print(bufStr);
 }
 
 TFTDisplay::~TFTDisplay()
